468c8c217ae944bb59c07573e3fa359b9866d208
wtbtabuo <keita753@gmail.com>
2024-01-28 12:00:35 +0900

    パケット細分化、ファイルアップロード関数作成

--- VideoCompressor/VideoCompressor/client.py
+++ VideoCompressor/VideoCompressor/client.py
@@ -1,4 +1,5 @@
 import json
+import tkinter.filedialog
 
 from lib import utils
 
@@ -16,22 +17,38 @@ class Client:
         self.client_socket.close()
         print('connection closed')
 
-    def send_messages(self):
-        user_input = input('テスト入力です: ')
-        user_input = json.dumps(user_input)
+    def send_messages(self, data):
+        user_input = json.dumps(data)
         self.client_socket.sendto(user_input.encode(), (self.address, self.port))
 
     def receive_messages(self):
         while True:
-            data, address = self.client_socket.recv((self.address, self.port))
+            data, address = self.client_socket.recv(4096)
             if data:
                 data = data.decode()
                 print(json.dumps(data))
+
+    def split_packt(self, data, packet_size=1400):
+        # データをパケットサイズに分割して送信
+        for i in range(0, len(data), packet_size):
+            packet = data[i:i+packet_size]
+            self.send_messages(packet)
+    @staticmethod
+    def upload_mp4_fine():
+        init_dir = '/mnt/c/Users/keita/Downloads/'
+        target_file = tkinter.filedialog.askopenfilename(
+            initialdir=init_dir,
+            filetypes=[("mp4ファイル", "*.mp4")]
+            )
+        return target_file
 if __name__ == '__main__':
     client = Client()
     try:
         client.connect()
-        client.send_messages()
+        mp4_data = client.upload_mp4_fine()
+        print(len(mp4_data))
+        # client.split_packt(user_input)
+        # client.receive_messages()
     finally:
         client.disconnect()
     
