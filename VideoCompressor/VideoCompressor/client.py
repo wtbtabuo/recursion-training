2565ccc633361822d89a4028df6e481c98c8531b
wtbtabuo <keita753@gmail.com>
2024-01-29 22:53:18 +0900

    動画アップロード後の処理追加

--- VideoCompressor/VideoCompressor/client.py
+++ VideoCompressor/VideoCompressor/client.py
@@ -32,7 +32,7 @@ class Client:
             data = self.client_socket.recv(4096)
             if data:
                 data = data.decode()
-                print(json.loads(data))
+                return json.loads(data)
 
     def choose_and_check_mp4_file(self):
         # アップロードするファイル選択と、ファイルサイズが4GB以下かどうかのバリデーション
@@ -54,7 +54,7 @@ class Client:
 
             return target_file, file_size
     
-    def generate_tcp_packet(self, path, size):
+    def send_tcp_packet(self, path, size):
         data = {
             'file_name': path.split('/')[-1],
             'file_size': size
@@ -67,16 +67,40 @@ class Client:
                 if not data:
                     break
                 self.send_bin_messages(data)
-        print('送信完了しました')
+        print('送信中...')
 
+        # ステータスコード200が返ってくるまで待機
+        if self.receive_messages().get('status_code') == 200:
+            print('動画の送信に成功しました')
+        else:
+            print('動画の送信に失敗しました')
+        
+    def choose_operation(self):
+        operation = input("""
+        動画処理について選択してください:
+        1. 動画を圧縮する
+        2. 動画の解像度を変更する
+        3. 動画のアスペクト比を変更する
+        4. 動画を音声に変換する
+        5. 指定した時間範囲でGIF、若しくはWEBMを作成
+        選択: """)
+        while True:
+            if int(operation) not in [1, 2, 3, 4, 5]:
+                print('1～5を入力して下さい')
+            else:
+                break
+        
+        if operation == 1:
+            req = {'operation_id': 1}
+            self.send_str_messages(req)
+        
 if __name__ == '__main__':
     client = Client()
     try:
         client.connect()
         file_path, file_size = client.choose_and_check_mp4_file()
-        if file_path and file_size:
-            client.generate_tcp_packet(file_path, file_size)
-        client.receive_messages()
+        client.send_tcp_packet(file_path, file_size)
+        client.choose_operation()
     finally:
         client.disconnect()
     
